658de166d065f6063ce1cc5c4ea63dc75fe886cd
gmkleinman <gmkleinman@gmail.com>
2021-07-31 11:28:12 -0700

    removed long polling

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+		transports: ['websocket'],
+});
 const port = process.env.PORT || 8080;
 
+// "must only use the websocket"
+// io.set('transports', ['websocket']);
 app.get('/', (req, res) => {
 	res.status(200).sendFile(__dirname + '/index.html');
 });
