3464e36acdde9bc9380ef5f8c812c4dc869e4f8a
gmkleinman <gmkleinman@gmail.com>
2021-07-31 21:43:06 -0700

    server messages and user count

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
-		transports: ['websocket'],
+	transports: ['websocket'],
 });
 const port = process.env.PORT || 8080;
 
@@ -12,15 +12,30 @@ app.get('/', (req, res) => {
 	res.status(200).sendFile(__dirname + '/index.html');
 });
 
-
 io.on('connection', (socket) => {
+	let count = io.engine.clientsCount;
+	io.emit('user connected', count);
+	io.emit('chat message', {
+		user: 'server',
+		message: 'user connected'
+	});
+
+	console.log(count)
 	console.log('a user connected');
 	socket.on('disconnect', () => {
+		count = io.engine.clientsCount;
+
+		io.emit('chat message', {
+			user: 'server',
+			message: 'user disconnected'
+		});
+		io.emit('user disconnected', count);
 		console.log('user disconnected');
 	});
+
 	socket.on('chat message', (msg) => {
 		io.emit('chat message', msg);
-	  });
+	});
 });
 
 http.listen(port, () => console.log('listening on port ' + port));
